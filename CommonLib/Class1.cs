b98285026551c8e2e1acda3a598a6bae485a77a0
Chris <3ch3l0n3@gmail.com>
2022-04-19 17:41:09 +0200

    Modules Conectivity tests

--- CommonLib/Class1.cs
+++ CommonLib/Class1.cs
@@ -1,5 +1,8 @@
 ﻿namespace CommonLib;
 public class Class1
 {
-
+    public string about()
+    {
+        return "pozdro z klasy libow!";
+    }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using CommonLib;
+
 Console.WriteLine("Hello, World!");
+var libClass = new Class1();
+
+Console.WriteLine(libClass.about());
