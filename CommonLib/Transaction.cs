8d0d69fe0b13c709200881b597132bc82a24ca8d
Chris <3ch3l0n3@gmail.com>
2022-06-27 14:38:35 +0200

    transactionclass

---
+++ CommonLib/Transaction.cs
@@ -0,0 +1,12 @@
+/*
+Klasa zawierająca encje transakcji
+!!! Uwaga !!! Niebezpieczeństwo tu śpią smoki
+? Niewiadomow o co chodzi
+todo kł: ZERKNIJ NA TO 
+*/
+
+class Transaction
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
